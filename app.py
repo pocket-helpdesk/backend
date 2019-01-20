2a0605888718524f5dcd8d408f16bbbbf8f190bf
Jose Diaz <jrd.palacios@gmail.com>
2019-01-19 18:01:13 -0600

    Initial DJango app

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 #/usr/bin/even python3
 
-def main():
-	print('Pocket Helpdesk')
-	# TODO: Build a DJango app
+import sys
 
-if __name__ == '__main__':
-	main()
+from django.conf.urls import url
+from django.http import HttpResponse
+from django.conf import settings
+
+def index(request):
+	return HttpResponse('Hello World')
+
+urlpatterns = (
+    url(r'^$', index),
+)
+
+
+settings.configure(
+	DEBUG=True,
+	SECRET_KEY='thisisthesecretkey',
+	ROOT_URLCONF=__name__,
+	MIDDLEWARE_CLASSES=(
+		'django.middleware.common.CommonMiddleware',
+		'django.middleware.csrf.CsrfViewMiddleware',
+		'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	),
+)
+
+if __name__ == "__main__":
+	from django.core.management import execute_from_command_line
+	execute_from_command_line(sys.argv)
